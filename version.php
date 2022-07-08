058f3602be9f8c7fff6349c106fe40799fa27891
Carlos J. Palacios <cjpm1983@gmail.com>
2022-07-08 08:49:05 -0400

    updated version

--- version.php
+++ version.php
@@ -23,6 +23,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2020061500;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2022070800;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2020060900;        // Requires this Moodle version
 $plugin->component = 'profilefield_orcid'; // Full name of the plugin (used for diagnostics)
